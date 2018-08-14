2d06a296c03bec49f3a8c62a44aafa45ae0cf768
erikamorais <erikamorais2001@gmail.com>
2018-08-14 17:17:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/name" do 
+  "Hello my name is Erika!"
+  end
+  
+  get "/hometown" do
+    "My hometown is New York"
+  end 
+  
+  get "/song" do
+    "My favorite song is idek"
+  end
+  
 end
